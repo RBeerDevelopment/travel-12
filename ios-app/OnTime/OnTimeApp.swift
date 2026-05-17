f429eb01e815a6a098235bd97e112d3a9bbf0f00
Robin Beer <contact@robin-beer.com>
2026-05-17 09:11:34 +0200

    setup sentry with custom dsn

--- ios-app/OnTime/OnTimeApp.swift
+++ ios-app/OnTime/OnTimeApp.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import Sentry
+
 import SwiftData
 import Toasts
 
@@ -14,6 +16,29 @@ struct OnTimeApp: App {
     @State private var modelContainer: ModelContainer
     
     init() {
+        SentrySDK.start { options in
+            options.dsn = Bundle.main.object(forInfoDictionaryKey: "SentryDSN") as? String
+
+            // Adds IP for users.
+            // For more information, visit: https://docs.sentry.io/platforms/apple/data-management/data-collected/
+            options.sendDefaultPii = true
+
+            options.tracesSampleRate = 0
+
+            // Configure profiling. Visit https://docs.sentry.io/platforms/apple/profiling/ to learn more.
+            options.configureProfiling = {
+                $0.sessionSampleRate = 1.0 // We recommend adjusting this value in production.
+                $0.lifecycle = .trace
+            }
+
+            // Uncomment the following lines to add more data to your events
+            // options.attachScreenshot = true // This adds a screenshot to the error events
+            // options.attachViewHierarchy = true // This adds the view hierarchy to the error events
+            
+            // Enable experimental logging features
+            options.experimental.enableLogs = true
+        }
+
         modelContainer = try! ModelContainer(for: FavoriteTrip.self, RecentSearchStation.self)
         FavoritesManager.configure(context: modelContainer.mainContext)
     }
